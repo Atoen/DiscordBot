700043049b871245ef5b35dab055dd1ca1a06aff
Artur B <bachtaartur02@gmail.com>
2022-04-22 01:09:52 +0200

    Boting

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBot;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private readonly DiscordSocketClient _client;
+    private readonly CommandService _commands;
+    private readonly IServiceProvider _services;
+
+    private Program()
+    {
+        _client = new DiscordSocketClient(new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Info,
+            MessageCacheSize = 100
+        });
+        
+        _commands = new CommandService(new CommandServiceConfig
+        {
+            LogLevel = LogSeverity.Info,
+            CaseSensitiveCommands = false
+        });
+        
+        _client.Log += Log;
+        _commands.Log += Log;
+        
+        _services = ConfigureServices();
+    }
+
+    private async Task MainAsync()
+    {
+        var token = "";
+
+        await InitCommands();
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(Timeout.Infinite);
+    }
+    
+    private static IServiceProvider ConfigureServices()
+    {
+        var map = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+
+        return map.BuildServiceProvider();
+    }
+
+    private async Task InitCommands()
+    {
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        
+        _client.MessageReceived += HandleCommandAsync;
+    }
+    
+    private async Task HandleCommandAsync(SocketMessage arg)
+    {
+        if (arg is not SocketUserMessage message || message.Author.IsBot) return;
+
+        var argPos = 0;
+        if (message.HasStringPrefix("!", ref argPos))
+        {
+            Console.WriteLine(message.Content);
+            
+            var context = new SocketCommandContext(_client, message);
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+            {
+                await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+    }
+    
+    private static Task Log(LogMessage message)
+    {
+        switch (message.Severity)
+        {
+            case LogSeverity.Critical:
+            case LogSeverity.Error:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case LogSeverity.Warning:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case LogSeverity.Info:
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            case LogSeverity.Verbose:
+            case LogSeverity.Debug:
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                break;
+        }
+        
+        Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}");
+        Console.ResetColor();
+        
+        return Task.CompletedTask;
+    }
+}
